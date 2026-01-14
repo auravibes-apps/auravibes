b0215f9d03fb1fb6642e729197a3a310a04704e3
David Alejandro Londoño <davidlondono@users.noreply.github.com>
2026-01-13 21:06:06 -0500

    fix naming bundle (#113)

--- apps/auravibes_app/android/app/build.gradle.kts
+++ apps/auravibes_app/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ val keystoreProperties =
 
 
 android {
-    namespace = "com.example.auravibes"
+    namespace = "me.auravibes.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
     
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.auravibes"
+        applicationId = "me.auravibes.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
