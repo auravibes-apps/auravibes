f49c091863e2490791dc704923a1d1000e2d31b6
David Londono <davidlondono9@gmail.com>
2026-03-10 10:28:59 -0500

    docs: document experimental_member_use ignore in widgetbook

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: experimental_member_use
+// Required: Project uses sealed classes (experimental) which are referenced by widgetbook
 import 'package:auravibes_ui/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
