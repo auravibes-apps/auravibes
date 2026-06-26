addf5cf0957144eb0ebf4bfc0ef5f1f6155d7852
David Londono <davidlondono9@gmail.com>
2026-06-25 20:22:07 -0500

    style(providers): format barrel export line

--- packages/auravibes_genkit_providers/lib/auravibes_genkit_providers.dart
+++ packages/auravibes_genkit_providers/lib/auravibes_genkit_providers.dart
@@ -1,4 +1,5 @@
-export 'src/chat_completions_provider.dart' show ApiKeyProvider, ChatCompletionsModelDefinition;
+export 'src/chat_completions_provider.dart'
+    show ApiKeyProvider, ChatCompletionsModelDefinition;
 export 'src/openai_codex.dart';
 export 'src/openai_compat_chat_options.dart';
 export 'src/openai_compat_reasoning.dart';
