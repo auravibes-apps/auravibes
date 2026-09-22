c61756ed45cd2cdbba1fa397eaccde973d0146dd
David Alejandro Londoño <davidlondono@users.noreply.github.com>
2026-09-22 13:00:00 -0500

    chore(devx): stop cloning dev databases (#959)

--- scripts/start_windows_app.cmd
+++ scripts/start_windows_app.cmd
@@ -3,8 +3,5 @@ setlocal
 
 for /f "delims=" %%I in ('git rev-parse --show-toplevel') do set "WORKTREE=%%I"
 
-fvm dart run tool/clone_database.dart --hash-source "%WORKTREE%"
-if errorlevel 1 exit /b %errorlevel%
-
 cd /d "%WORKTREE%\apps\auravibes_app"
 fvm flutter run -d windows --flavor dev --dart-define=AURAVIBES_SERVER_URL=http://localhost:8080/ "--dart-define=DB_HASH_SOURCE=%WORKTREE%"
