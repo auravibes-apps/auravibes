37b7eaf813b2660699e3bf70588b888b543d7248
David Londono <davidlondono9@gmail.com>
2026-09-24 10:22:07 -0500

    test: honor CI widget test timeout

---
+++ widgetbook/test/flutter_test_config.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+import '../../tool/testing/ci_widget_test_timeout.dart';
+
+Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  configureCiWidgetTestTimeout();
+  await testMain();
+}
